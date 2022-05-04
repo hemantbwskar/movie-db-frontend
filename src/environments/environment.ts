4c0fa2ba9315d6971019b06d36a3e7a5cc83f859
hemantbwskar <86668867+hemantbwskar@users.noreply.github.com>
2022-05-04 15:34:28 +0530

    new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   
   BASE_URL: 'http://localhost:5000/',
   AUTH_URL: 'http://localhost:5000/authenticate/',
